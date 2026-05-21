10d65cb234fcf6e118b9a988c46d51147df3c40e
Jit9763 <104998314+Jit9763@users.noreply.github.com>
2026-05-21 11:44:07 +0530

    Update DEFAULT_API_URL in script.js to the new deployed Web App URL

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ const valueLabelPlugin = {
     }
 };
 Chart.register(valueLabelPlugin);
-const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbwmB3xnoQrGdWTnUo7Dm_9bFb6L1I_qDo7Nkw2IYoladOtuK1fsh3wfYyo_kXJ_N4f3wA/exec';
+const DEFAULT_API_URL = 'https://script.google.com/macros/s/AKfycbwKJWLEMINBxrtOZxzCS9ByF36zuaUW7Xb1VdsOJV4ULsUPnnCe462PZiSCSNes5F3ySg/exec';
 const isAdminPage = window.location.pathname.includes('admin.html');
 
 let activeCensusTab = 'progress'; // 'progress' | 'directory'
